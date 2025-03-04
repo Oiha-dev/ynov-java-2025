5da6b3924e412ac2309b3239b0d781a84ca8478d
oiha <cenes.gauthier@gmail.com>
2025-03-04 16:35:04 +0100

    feat(Arrays): add Arrays class with a simple for-each loop

---
+++ src/fr/ynov/java/easy/Arrays.java
@@ -0,0 +1,11 @@
+package fr.ynov.java.easy;
+
+public class Arrays {
+    public static void main(String[] args) {
+        int[] array = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
+
+        for (int j : array) {
+            System.out.println(j);
+        }
+    }
+}
