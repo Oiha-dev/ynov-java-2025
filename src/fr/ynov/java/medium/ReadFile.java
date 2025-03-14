f5f1ade719c89368d282295a620f62dc1da7c77d
oiha <cenes.gauthier@gmail.com>
2025-03-14 14:37:55 +0100

    feat(ReadFile): add ReadFile class to read and print contents of files in a directory

---
+++ src/fr/ynov/java/medium/ReadFile.java
@@ -0,0 +1,19 @@
+package fr.ynov.java.medium;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
+
+public class ReadFile {
+    public static void main(String[] args) throws FileNotFoundException {
+        File folder = new File("test");
+        for (File file : folder.listFiles()) {
+            Scanner myReader = new Scanner(file);
+            while (myReader.hasNextLine()) {
+                String data = myReader.nextLine();
+                System.out.println(data);
+            }
+            myReader.close();
+        }
+    }
+}
